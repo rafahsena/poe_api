7243c93c692d72d976f44b4d2f68c0e9133bd585
Rafael Sena <rafaelsenna13@gmail.com>
2020-01-24 12:51:16 -0300

    Added bulk_update action

--- currencies/api/serializers.py
+++ currencies/api/serializers.py
@@ -1,9 +1,24 @@
 from rest_framework import serializers
 from currencies.models import Currency
 
-class CurrencySerializer(serializers.ModelSerializer):
+class CurrencyListSerializer(serializers.ListSerializer):
+    def update(self, instance, validated_data):
+        print(validated_data)
+        currency_mapping = {currency.slug: currency for currency in instance}
+        data_mapping = {item['slug']: item for item in validated_data}
+        
+        ret = []
+        for currency_slug, data in data_mapping.items():
+            currency = currency_mapping.get(currency_slug, None)
+            if currency:
+                ret.append(self.child.update(currency, data))
 
+        return ret
+
+class CurrencySerializer(serializers.ModelSerializer):
+    slug = serializers.SlugField()
     class Meta:
+        list_serializer_class = CurrencyListSerializer
         model = Currency
-        fields = ('id', 'slug', 'name', 'value', 'change')
+        fields = ('slug', 'name', 'value', 'change')
         read_only_fields = ('id', 'slug')
--- currencies/api/viewsets.py
+++ currencies/api/viewsets.py
@@ -1,8 +1,10 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.authentication import TokenAuthentication
-from currencies.api.serializers import CurrencySerializer
+from currencies.api.serializers import CurrencySerializer, CurrencyListSerializer
 from currencies.models import Currency
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 class BearerAuthentication(TokenAuthentication):
     keyword = 'Bearer'
@@ -12,4 +14,23 @@ class CurrencyViewSet(viewsets.ModelViewSet):
     serializer_class = CurrencySerializer
     permission_classes = (IsAuthenticatedOrReadOnly, )
     authentication_classes = (BearerAuthentication, )
-    
+
+    def get_serializer(self, *args, **kwargs):
+        if "data" in kwargs:
+            data = kwargs["data"]
+
+            # check if many is 
+            print("oi")
+            if isinstance(data, list):
+                kwargs["many"] = True
+                print('alooooooo')
+
+        return super().get_serializer(*args, **kwargs)
+
+    @action(methods=['put'], detail=False)
+    def bulk_update(self, request):
+        qs = self.get_queryset()
+        serializer = self.get_serializer(data=request.data, instance=qs, many=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
