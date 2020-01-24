4d6f400b18b5041364872f370d7c1e6bafcad388
Rafael Sena <rafaelsenna13@gmail.com>
2020-01-24 12:49:52 -0300

    Added core app to project

--- poe_api/settings.py
+++ poe_api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'currencies',
+    'core',
     'rest_framework',
     'rest_framework.authtoken',
 ]
@@ -121,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10
+}
